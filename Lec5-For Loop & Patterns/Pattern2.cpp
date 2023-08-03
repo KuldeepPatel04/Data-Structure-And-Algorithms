abccd5519f2075f5e08a3339ca2aa5fd3064f38d
KuldeepPatel04 <kalariyakuldeep8238@gmail.com>
2023-08-03 19:30:48 +0530

    Hollow Square Pattern
    * * * * *
    *        *
    * * * * *

---
+++ Lec5-For Loop & Patterns/Pattern2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<3;i++)
+    {
+        if(i==0 || i==2)
+        {
+            for(int j=0;j<5;j++)
+            {
+                cout << " * ";
+            }
+        }
+        else
+        {
+            cout << " * ";
+            for (int i=0;i<3;i++)
+            {
+                cout << "   " ;
+            }
+            cout << " * ";
+        }
+        cout << endl;
+    }
+}
+
+//keep eye while giving extra space for good look
