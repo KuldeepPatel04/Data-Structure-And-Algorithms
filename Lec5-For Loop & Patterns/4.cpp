3cf225caca1536bb041a7000ecd795a2bdc8860f
KuldeepPatel04 <kalariyakuldeep8238@gmail.com>
2023-08-03 19:25:03 +0530

    Tricky Codes

---
+++ Lec5-For Loop & Patterns/4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    if(cin >> i)
+    {
+        cout << "kuldeep" << endl;
+    }      
+    int j;
+    if(cout << "nothing")
+    {
+        cout << "kalariya" << endl;
+    }
+}
