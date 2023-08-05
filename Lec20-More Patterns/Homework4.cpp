00e21b8ed31f88793b1e11e42f88a050c48eb54f
KuldeepPatel04 <kalariyakuldeep8238@gmail.com>
2023-08-05 16:44:03 +0530

    Solid Half Diamond
    *
    **
    ***
    ****
    *****
    ****
    ***
    **
    *

---
+++ Lec20-More Patterns/Homework4.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k;
+    cin >> k;
+    for(int i=0;i<k;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=0;i<k-1;i++)
+    {
+        for(int j=0;j<k-i-1;j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
