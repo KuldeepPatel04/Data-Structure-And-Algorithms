0abfd66b681cd19588c7c93793b7b7067554a95c
KuldeepPatel04 <kalariyakuldeep8238@gmail.com>
2023-08-08 18:51:40 +0530

    Trick Codes

---
+++ Lec22-Functions/2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printnum(int a)
+{
+    a++;
+    cout << a << endl;
+
+}
+int main()
+{
+    int a=1;
+    printnum(a);
+    a--;
+    cout << a << endl;
+    printnum(a);
+    return 0;
+}
