139a9780c157f4c0b61e025d1bbf0a97b7e74de6
KuldeepPatel04 <kalariyakuldeep8238@gmail.com>
2023-08-08 18:51:26 +0530

    Simple Function

---
+++ Lec22-Functions/1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void printname()
+{
+    int n;
+    cout << " enter value " ;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        cout << "kuldeep" << endl;
+    }
+}
+int main()
+{
+       printname();
+}
+
+//Functions can be data types
