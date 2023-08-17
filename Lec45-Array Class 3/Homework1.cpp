ddf37940d9c171aebafd05fe8720c1b7b05ff918
KuldeepPatel04 <kalariyakuldeep8238@gmail.com>
2023-08-17 23:47:30 +0530

    Find Sum Of Colums In 2D Array

---
+++ Lec45-Array Class 3/Homework1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void printrowsum(int arr[][4],int row,int cols)
+{
+    for(int i=0;i<4;i++)
+    {
+        int sum=0;
+        for(int j=0;j<4;j++)
+        {
+            sum=sum+arr[j][i];
+        }
+        cout <<"sum is " << sum << endl;
+    }
+}
+int main()
+{
+    int arr[4][4];
+    int row=4,colm=4;
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
+    printrowsum(arr,row,colm);
+}
