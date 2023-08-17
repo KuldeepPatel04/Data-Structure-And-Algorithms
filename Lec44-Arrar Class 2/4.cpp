319b5f60240bd201370efff3b4ab224dbd717094
KuldeepPatel04 <kalariyakuldeep8238@gmail.com>
2023-08-17 21:34:41 +0530

    Finding Same Numbers In Two Arrays

--- Lec44-Arrar Class 2/4.cpp
+++ Lec44-Arrar Class 2/4.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
 #include<vector>
+#include<limits.h>
 using namespace std;
 int main()
 {
-    int arr[4]={1,2,3,4};
-    int brr[3]={2,3,4};
+    int arr[4]={1,2,3,3};
+    int brr[3]={3,3,4};
     vector<int>ans;
     for(int i=0;i<4;i++)
     {
         for(int j=0;j<3;j++)
         {
             if(arr[i]==brr[j])
+            {
             ans.push_back(arr[i]);
+            brr[j]=INT_MIN;
+            }
         }
     }
     for(auto value:ans)
